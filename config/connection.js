280f8658a8d864588988220725821d419e948436
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-03-15 19:18:57 -0400

    fixed typo in connection js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_Name,
+  process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
