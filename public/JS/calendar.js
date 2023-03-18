8c856bd7c62c450ed5f0a0eaf9de7707ae4c1ab9
mtstall <mtstallings95@gmail.com>
2023-03-18 14:40:57 -0400

    working on add event functionality

--- public/JS/calendar.js
+++ public/JS/calendar.js
@@ -17,3 +17,32 @@ const calendarEl = document.getElementById("calendar");
 
 calendar.render();
 }) ()
+
+// add new event
+async function newEventHandler(event) {
+  event.preventDefault();
+  const event_name = document.querySelector('#event-name').value;
+  const description = document.querySelector('#event-description').value;
+  const startTime = document.querySelector('#event-start-time').value;
+  const endTime = document.querySelector('#event-end-time').value;
+
+  const response = await fetch('/api/calendar', {
+    method: 'POST',
+    body: JSON.stringify({
+      event_name,
+      description,
+      startTime,
+      endTime,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/calendar');
+  } else {
+    alert('Failed to add event');
+  }
+
+}
