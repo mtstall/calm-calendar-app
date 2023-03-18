4403f4c4dd1661748fbb090e9216f2acda985b91
mtstall <mtstallings95@gmail.com>
2023-03-18 14:34:39 -0400

    rendering calendar with user's events

--- public/JS/calendar.js
+++ public/JS/calendar.js
@@ -1,11 +1,19 @@
 //calendar page using FullCalender.io
-
+let userEvents = [];
 const calendarEl = document.getElementById("calendar");
-const calendar = new FullCalendar.Calendar(calendarEl, {
-  initialView: "dayGridMonth",
-  editable: true, // important for activating event interactions!
-  selectable: true, // important for activating date selectability!
-  events: [{ title: "Meeting", start: new Date() }],
-});
+
+(async function () {
+  const response = await fetch("/api/calendar");
+  const data = await response.json();
+  const formattedEvents = data.events.map((d) => {
+    return { title: d.event_name, start: d.startTime.slice(0, 10) };
+  });
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+    editable: true, // important for activating event interactions!
+    selectable: true, // important for activating date selectability!
+    events: formattedEvents,
+  });
 
 calendar.render();
+}) ()
